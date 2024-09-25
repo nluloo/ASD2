6da9cc6ff5fb5fa133cb65dcc1005dbbc789e137
kirill <kirillsegrenev5@gmail.com>
2024-09-25 18:27:56 +0300

    lab4 add

--- lab3/lab3/lab3.cpp
+++ lab3/lab3/lab3.cpp
@@ -1,113 +1,94 @@
 #include <iostream>
-#include <queue>
-
-#include <chrono>
+#include <limits>
 
 using namespace std;
 
-const char vertices[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
+const char vertices[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I' };
 
 struct GRAPH {
-	int G[9][9] = { // A B C D E F G H I
-		{0, 7,10, 0, 0, 0, 0, 0, 0},
-		{7, 0, 0, 0, 0, 9,27, 0, 0},
-		{10,0, 0, 0,31, 8, 0, 0, 0},
-		{0, 0, 0, 0,32, 0, 0,17, 21},
-		{0, 0,31,32, 0, 0, 0, 0, 0},
-		{0, 9, 8, 0, 0, 0, 0,11, 0},
-		{0,27, 0, 0, 0, 0, 0, 0, 15},
-		{0, 0, 0,17, 0,11, 0, 0, 15},
-		{0, 0, 0,21, 0, 0,15,15, 0},
-	};
+    int G[9][9] = { // A B C D E F G H I
+        {0, 7,10, 0, 0, 0, 0, 0, 0},
+        {7, 0, 0, 0, 0, 9,27, 0, 0},
+        {10,0, 0, 0,31, 8, 0, 0, 0},
+        {0, 0, 0, 0,32, 0, 0,17, 21},
+        {0, 0,31,32, 0, 0, 0, 0, 0},
+        {0, 9, 8, 0, 0, 0, 0,11, 0},
+        {0,27, 0, 0, 0, 0, 0, 0,15},
+        {0, 0, 0,17, 0,11, 0, 0,15},
+        {0, 0, 0,21, 0, 0,15,15, 0},
+    };
 
-	bool mark[10];
-
-	GRAPH() {
-		for (int i = 0; i < 10; i++) {
-			mark[i] = false;
-		}
-	}
 };
 
-void Dekstr(GRAPH, int);
+int minDistance(int dist[], bool mark[]) {
+    int min = numeric_limits<int>::max();
+    int minIndex = -1;
 
-int main() {
-	setlocale(LC_ALL, "ru");
-	GRAPH g;
-	char select;
-	cout << "Введите вершину (A - I) | 0 - Завершить программу: ";
-	cin >> select;
-	
-	do
-	{
-		switch (select)
-		{
-		case 'A':
-			Dekstr(g, 0);
-			break;
-		case 'B':
-			Dekstr(g, 1);
-			break;
-		case 'C':
-			Dekstr(g, 2);
-			break;
-		case 'D':
-			Dekstr(g, 3);
-			break;
-		case 'E':
-			Dekstr(g, 4);
-			break;
-		case 'F':
-			Dekstr(g, 5);
-			break;
-		case 'G':
-			Dekstr(g, 6);
-			break;
-		case 'H':
-			Dekstr(g, 7);
-			break;
-		case 'I':
-			Dekstr(g, 8);
-			break;
-		default:
-			cout << "Error" << endl;
-		}
-		cout << "\nВведите вершину (A - I): ";
-		cin >> select;
-	} while (select != '0');
-	
+    for (int v = 0; v < 9; v++) {
+        if (!mark[v] && dist[v] <= min) {
+            min = dist[v];
+            minIndex = v;
+        }
+    }
 
-
-	return 0;
+    return minIndex;
 }
 
-void Dekstr(GRAPH g, int number = 0) { // ПЕРЕПРОВЕТИЬ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11
-	const int INF = numeric_limits<int>::max();
-	int dist[9] = { INF,INF,INF,INF ,INF,INF ,INF,INF ,INF};
+void Dekstr(GRAPH&, int);
 
-	dist[number] = 0;
-	queue<int> q;
-	q.push(number);
-	g.mark[number] = true;  // Отмечаем вершину как посещённую
+int main() {
+    setlocale(LC_ALL, "ru");
+    GRAPH g;
+    char select;
+    cout << "Введите вершину (A - I) | 0 - Завершить программу: ";
+    cin >> select;
 
-	while (!q.empty()) {
-		int u = q.front();  // Берём вершину из очереди
-		q.pop();
+    while (select != '0') {
+        switch (select) {
+        case 'A': Dekstr(g, 0); break;
+        case 'B': Dekstr(g, 1); break;
+        case 'C': Dekstr(g, 2); break;
+        case 'D': Dekstr(g, 3); break;
+        case 'E': Dekstr(g, 4); break;
+        case 'F': Dekstr(g, 5); break;
+        case 'G': Dekstr(g, 6); break;
+        case 'H': Dekstr(g, 7); break;
+        case 'I': Dekstr(g, 8); break;
+        default: cout << "Error" << endl;
+        }
+        cout << "\nВведите вершину (A - I): ";
+        cin >> select;
+    }
 
-		for (int v = 0; v < 9; v++) {
-			if (g.G[u][v] != 0 && !g.mark[v]) {  // Если есть связь и вершина не посещена
-				int newDist = dist[u] + g.G[u][v];
-				if (newDist < dist[v]) {  // Если нашли более короткий путь
-					dist[v] = newDist;
-					q.push(v);  // Добавляем вершину в очередь для дальнейшей обработки
-					g.mark[u] = true;
-				}
-			}
-		}
-	}
+    return 0;
+}
 
-	cout << "Расстояния от вершины " << vertices[number] << ":" << endl;
-	for (int i = 0; i < 9; i++) {
-		cout << "Вершина " << vertices[i] << ": " << dist[i] << endl;
-	}
+void Dekstr(GRAPH& g, int number) {
+    const int INF = numeric_limits<int>::max();
+    int dist[9];
+    bool sptSet[9]; 
+
+    for (int i = 0; i < 9; i++) {
+        dist[i] = INF;
+        sptSet[i] = false;
+    }
+
+    dist[number] = 0;
+
+    for (int count = 0; count < 9 - 1; count++) {
+        int u = minDistance(dist, sptSet); 
+        sptSet[u] = true;  // Помечаем вершину как пройденную
+
+        // Обновляем расстояния до соседей
+        for (int v = 0; v < 9; v++) {
+            if (!sptSet[v] && g.G[u][v] && dist[u] != INF && dist[u] + g.G[u][v] < dist[v]) {
+                dist[v] = dist[u] + g.G[u][v];
+            }
+        }
+    }
+
+    cout << "Расстояния от вершины " << vertices[number] << ":" << endl;
+    for (int i = 0; i < 9; i++) {
+            cout << "Вершина " << vertices[i] << ": " << dist[i] << endl;
+    }
 }
---
+++ lab4/lab4/lab4.cpp
@@ -0,0 +1,132 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <iomanip>
+
+using namespace std;
+
+const int INF = numeric_limits<int>::max();
+
+struct StructMatrix
+{
+	int D[6][6] = {
+		{0, 28, 21, 59, 12, 27},
+		{7,  0, 24, INF, 21, 9},
+		{9, 32, 0, 13, 11, INF},
+		{8, INF, 5, 0, 16, INF},
+		{14, 13, 15, 10, 0, 22},
+		{15, 18, INF, INF, 6, 0}
+	};
+	int S[6][6] = {
+		{0, 2, 3, 4, 5, 6},
+		{1, 0, 3, 4, 5, 6},
+		{1, 2, 0, 4, 5, 6},
+		{1, 2, 3, 0, 5, 6},
+		{1, 2, 3, 4, 0, 6},
+		{1, 2, 5, 5, 5, 0}
+	};
+
+	void ReturnMatrix() {
+		printf("Матрица расстояний:\n");
+		for (int i = 0; i < 6; i++)
+		{
+			for (int j = 0; j < 6; j++)
+				cout << setw(5) << D[i][j];
+			printf("\n");
+		}
+	}
+	void ReturnEdge() {
+		printf("Матрица путей:\n");
+		for (int i = 0; i < 6; i++)
+		{
+			for (int j = 0; j < 6; j++)
+				cout << setw(5) << S[i][j];
+			printf("\n");
+		}
+	}
+
+	void WayTo(int start, int finish) {
+		cout << "\nПуть из точки " << start << " в точку " << finish << ": ";
+		finish--;
+		cout << start-- << " -> ";
+
+		for (int i = start; S[i][finish] - 1 != finish; i = S[i][finish] - 1) {
+			cout << S[i][finish] << " -> ";
+		}
+		cout << ++finish << endl;
+	}
+
+	void printDistanceTo(int start, int finish) {
+		cout << "\nРасстояние из точки " << start << " в точку " << finish << ": ";
+		cout << D[--start][--finish] << endl;
+	}
+
+};
+
+void floyd(StructMatrix& dist) {
+	for (int v = 0; v < 6; v++) {
+		for (int i = 0; i < 6; i++) {
+			if (dist.D[i][v] != INF) {
+				for (int j = 0; j < 6; j++) {
+					if (dist.D[v][j] != INF && dist.D[i][j] > dist.D[i][v] + dist.D[v][j]) {
+						dist.D[i][j] = dist.D[i][v] + dist.D[v][j];
+						dist.S[i][j] = v + 1;
+					}
+				}
+			}
+		}
+	}
+}
+
+
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	StructMatrix a;
+	int value;
+	int start = 0, finish = 0;
+
+	cout << "1. Применить алгоритм Флойда-Уоршелла" << endl;
+	cout << "2. Вывести матрицу кратчайших расстояний" << endl;
+	cout << "3. Вывести матрицу путей" << endl;
+	cout << "4. Вывести путь из одной вершины в другую" << endl;
+	cout << "5. Вывести расстояние пути из одной вершины в другую" << endl;
+	cout << "0. Завершение программы" << endl;
+
+	cin >> value;
+	while (value != 0)
+	{
+		switch (value)
+		{
+		case 1:
+			floyd(a);
+			cout << "Алгоритм успешно применен!" << endl;
+			break;
+		case 2:
+			a.ReturnMatrix();
+			break;
+		case 3:
+			a.ReturnEdge();
+			break;
+		case 4:
+			cout << "Введите начальную вершину: ";
+			cin >> start;
+			cout << "Введите конучную вершину: ";
+			cin >> finish;
+			a.WayTo(start, finish);
+			break;
+		case 5:
+			cout << "Введите начальную вершину: ";
+			cin >> start;
+			cout << "Введите конучную вершину: ";
+			cin >> finish;
+			a.printDistanceTo(start, finish);
+			break;
+		default:
+			cout << "error" << endl;
+			break;
+		}
+		cin >> value;
+	}
+
+	return 0;
+}
